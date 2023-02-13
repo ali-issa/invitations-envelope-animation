3b5da75f9c501bffa026b5fde52f077d49646f30
Ali Issa <ali.issa.lb@gmail.com>
2023-02-14 02:42:22 +0400

    Cleanup and add comments

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+// Toggle animation on click
 document.querySelector(".envelope").addEventListener("click", function () {
+  // Check if the envelope has the class "fold"
   let hasFold = document.querySelector(".envelope").classList.contains("fold");
 
+  // Scale the paper up or down | Toggle the class "grow" after 1 second if the envelope has the class "fold" or immediately if it doesn't
   setTimeout(
     function () {
       document.querySelector(".envelope").classList.toggle("grow");
@@ -8,6 +11,7 @@ document.querySelector(".envelope").addEventListener("click", function () {
     hasFold ? 1000 : 0
   );
 
+  // Open or closed the envelope | Toggle the class "fold" immediately if the envelope has the class "fold" or after 1.5 seconds if it doesn't
   setTimeout(
     function () {
       document.querySelector(".envelope").classList.toggle("fold");
@@ -16,10 +20,12 @@ document.querySelector(".envelope").addEventListener("click", function () {
   );
 });
 
+// Animates the envelope folding on page load
 setTimeout(function () {
   document.querySelector(".envelope").classList.remove("fold");
 }, 500);
 
+// Animates the paper scale on page load
 setTimeout(function () {
   document.querySelector(".envelope").classList.add("grow");
 }, 1000);
